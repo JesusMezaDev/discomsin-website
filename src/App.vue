77784c8f17b99e0356e86617c35cbe7c091160a7
Jesús Meza <jame181185@gmail.com>
2024-01-11 16:47:03 -0700

    Se agrega el componente del footer

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,18 @@
   <div class="container">
     <Navbar />
     <RouterView />
-    <!-- <RouterView v-slot="{ Component, route }">
-      <KeepAlive>
-        <component :is="Component" :key="route.path" />
-      </KeepAlive>
-    </RouterView> -->
+    <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
+  import Footer from './components/Footer.vue';
   import Spinner from '@/shared/spinner/Spinner.vue';
   import Navbar from '@/modules/navbar/views/NavbarView.vue';
 </script>
 
 <style scoped>
-  
+  .text-10 {
+    font-size: 10px;
+  }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer class="bg-dark text-white w-100 text-10 p-2 footer">
+      <div class="row">
+        <div class="col">
+          Las imágenes que se muestran en este sitio son para fines ilustrativos.
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          {{ new Date().getFullYear() }} Discomsin
+        </div>
+      </div>
+    </footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+  .footer {
+    position: static;
+    bottom: 0;
+    left: 0;
+  }
+</style>
