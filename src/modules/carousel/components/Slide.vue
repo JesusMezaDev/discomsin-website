091ba880892d2a53b35e29d59074219f135e063a
Jesús Meza <jame181185@gmail.com>
2024-01-12 12:32:12 -0700

    Se ajusta el alto de las imágenes del carousel según la pantalla

--- src/modules/carousel/components/Slide.vue
+++ src/modules/carousel/components/Slide.vue
@@ -7,5 +7,39 @@
 </script>
 
 <style scoped>
+    @media screen and (max-width: 912px) {
+        img {
+            height: 220px;
+        }
+    }
     
+    @media screen and (max-width: 768px) {
+        img {
+            height: 200px;
+        }
+    }
+    
+    @media screen and (max-width: 540px) {
+        img {
+            height: 175px;
+        }
+    }
+
+    @media screen and (max-width: 414px) {
+        img {
+            height: 150px;
+        }
+    }
+
+    @media screen and (max-width: 375px) {
+        img {
+            height: 120px;
+        }
+    }
+
+    @media screen and (max-width: 280px) {
+        img {
+            height: 100px;
+        }
+    }
 </style>
