9563c5b8d66993b7d2ee12352f71bf3342e9ae26
Jesús Meza <jame181185@gmail.com>
2024-01-14 20:08:28 -0700

    Correcciones en los estilos para el visor de imágenes

--- src/shared/picture-viewer/views/PictureViewerView.vue
+++ src/shared/picture-viewer/views/PictureViewerView.vue
@@ -1,15 +1,13 @@
 <template>
     <div class="overlay" :class="{ 'active': showPictureInfo }" @click.self="showPictureInfo = false"></div>
     <div class="spinner" :class="{ 'active': showPictureInfo }">
-        <div class="bg">
-            <div class="card">
-                <img class="card-img-top mt-3 px-3" :src="pictureInfo.url" :alt="pictureInfo.title">
-                <div class="card-body" v-if="pictureInfo.title && pictureInfo.description">
-                    <h5 class="card-title">{{ pictureInfo.title }}</h5>
-                    <p>
-                        {{ pictureInfo.description }}
-                    </p>
-                </div>
+        <div class="card">
+            <img class="card-img-top mt-3 px-3" :src="pictureInfo.url" :alt="pictureInfo.title">
+            <div class="card-body" v-if="pictureInfo.title && pictureInfo.description">
+                <h5 class="card-title">{{ pictureInfo.title }}</h5>
+                <p class="card-text">
+                    {{ pictureInfo.description }}
+                </p>
             </div>
         </div>
     </div>
@@ -23,5 +21,44 @@
 </script>
 
 <style scoped>
+    @media screen and (min-width: 1280px) {
+        img {
+            height: 420px;
+            width: 880px;
+        }
+    }
     
+    @media screen and (max-width: 1024px) {
+        img {
+            height: 360px;
+            width: 720px;
+        }
+    }
+
+    @media screen and (max-width: 768px) {
+        img {
+            height: 300px;
+            width: 620px;
+        }
+    }
+    
+    @media screen and (max-width: 768px) {
+        img {
+            height: 240px;
+            width: 520px;
+        }
+    }
+    
+    @media screen and (max-width: 480px) {
+        img {
+            height: 130px;
+            width: 280px;
+        }
+    }
+    
+    @media screen and (max-width: 280px) {
+        img {
+            height: 120px;
+        }
+    }
 </style>
